d59eca455ec5bae6849adb9d5526e9210dd8c482
Aivan <aish3n@pm.me>
2022-06-23 09:19:12 +0800

    feat: neovim plugnis

--- cfg/nvim/lua/plugins/init.lua
+++ cfg/nvim/lua/plugins/init.lua
@@ -1,31 +1,38 @@
 require("packer").startup(function(use)
     use("wbthomason/packer.nvim")
     use("gpanders/editorconfig.nvim")
-    use("elkowar/yuck.vim")
     use("godlygeek/tabular")
     use("challenger-deep-theme/vim")
 
-    use {
+    use({
         "lewis6991/impatient.nvim",
         config = {
             -- Move to lua dir so impatient.nvim can cache it
             compile_path = vim.fn.stdpath("config") .. "/lua/packer_compiled.lua"
         }
-    }
+    })
 
-    use {
+    use({
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup({
+                current_line_blame = true,
+            })
+        end
+    })
+
+    use({
         "andweeb/presence.nvim",
         config = function()
             require("presence"):setup()
         end
-    }
+    })
 
-    use {
+    use({
         "hrsh7th/nvim-cmp",
         requires = {
             "neovim/nvim-lspconfig",
             "ray-x/lsp_signature.nvim",
-
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
@@ -35,22 +42,22 @@ require("packer").startup(function(use)
         config = function ()
             require "plugins.configs.completion"
         end
-    }
+    })
 
-    use {
+    use({
         "L3MON4D3/LuaSnip",
         requires = "rafamadriz/friendly-snippets",
-    }
+    })
 
-    use {
+    use({
         "williamboman/nvim-lsp-installer",
         after = "nvim-cmp",
         config = function ()
             require "plugins.configs.lsp_installer"
         end
-    }
+    })
 
-    use {
+    use({
         "kyazdani42/nvim-tree.lua",
         config = function()
             require("nvim-tree").setup({
@@ -62,9 +69,9 @@ require("packer").startup(function(use)
                 },
             })
         end
-    }
+    })
     -- using packer.nvim
-    use {
+    use({
         "akinsho/bufferline.nvim",
         tag = "v2.*",
         requires = "kyazdani42/nvim-web-devicons",
@@ -91,5 +98,5 @@ require("packer").startup(function(use)
                 }
             })
         end
-    }
+    })
 end)
