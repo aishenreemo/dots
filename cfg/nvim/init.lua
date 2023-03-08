761a46e2d519183a5d9f1def873d95e6e69e9ade
Aivan <aish3n@pm.me>
2023-03-08 13:36:45 +0800

    feat: nvim tree open at startup

--- cfg/nvim/init.lua
+++ cfg/nvim/init.lua
@@ -8,3 +8,12 @@ require("core.packer")
 require("core.options")
 require("plugins.init")
 require("core.keybinds")
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { 
+    callback = function (data)
+        if vim.fn.isdirectory(data.file) == 1 then
+            vim.cmd.cd(data.file)
+            require("nvim-tree.api").tree.open()
+        end
+    end
+})
