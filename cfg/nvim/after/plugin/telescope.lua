818d10ac1e58c505617394ff8bb04162e9c86451
Aishen <aish3n@pm.me>
2023-03-25 16:50:46 +0800

    feat: telescope default mode -> normal

--- cfg/nvim/after/plugin/telescope.lua
+++ cfg/nvim/after/plugin/telescope.lua
@@ -1,7 +1,13 @@
 local telescope = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>fs", telescope.live_grep, {})
-vim.keymap.set("n", "<leader>fk", telescope.buffers, {})
-vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
-vim.keymap.set("n", "<leader>fj", telescope.find_files, {})
+local function live_grep() telescope.live_grep({ initial_mode = "normal" }) end
+local function buffers() telescope.buffers({ initial_mode = "normal" }) end
+local function help_tags() telescope.help_tags({ initial_mode = "normal" }) end
+local function find_files() telescope.find_files({ initial_mode = "normal" }) end
+local function find_files_hidden() telescope.find_files({ initial_mode = "normal", hidden = true }) end
 
+vim.keymap.set("n", "<leader>flg", live_grep, {})
+vim.keymap.set("n", "<leader>fb", buffers, {})
+vim.keymap.set("n", "<leader>f?", help_tags, {})
+vim.keymap.set("n", "<leader>ff", find_files, {})
+vim.keymap.set("n", "<leader>fh", find_files_hidden, {})
