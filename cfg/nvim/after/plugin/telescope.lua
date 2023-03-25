948fc82591d6677e957bcb4464a10185a594b6bf
Aishen <aish3n@pm.me>
2023-03-25 17:00:36 +0800

    feat: add ignored paths in telescope.nvim

--- cfg/nvim/after/plugin/telescope.lua
+++ cfg/nvim/after/plugin/telescope.lua
@@ -1,10 +1,20 @@
-local telescope = require("telescope.builtin")
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
 
-local function live_grep() telescope.live_grep({ initial_mode = "normal" }) end
-local function buffers() telescope.buffers({ initial_mode = "normal" }) end
-local function help_tags() telescope.help_tags({ initial_mode = "normal" }) end
-local function find_files() telescope.find_files({ initial_mode = "normal" }) end
-local function find_files_hidden() telescope.find_files({ initial_mode = "normal", hidden = true }) end
+telescope.setup({
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+            ".git"
+        }
+    }
+})
+
+local function live_grep() builtin.live_grep({ initial_mode = "normal" }) end
+local function buffers() builtin.buffers({ initial_mode = "normal" }) end
+local function help_tags() builtin.help_tags({ initial_mode = "normal" }) end
+local function find_files() builtin.find_files({ initial_mode = "normal" }) end
+local function find_files_hidden() builtin.find_files({ initial_mode = "normal", hidden = true }) end
 
 vim.keymap.set("n", "<leader>flg", live_grep, {})
 vim.keymap.set("n", "<leader>fb", buffers, {})
