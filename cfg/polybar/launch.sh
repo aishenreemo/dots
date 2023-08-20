e1267fe57c85b224b1e0a8d303906b51dafee696
Aishen <aish3n@pm.me>
2023-08-20 14:16:01 +0800

    feat(polybar): reload bar whenever the config file is changed

--- cfg/polybar/launch.sh
+++ cfg/polybar/launch.sh
@@ -8,4 +8,4 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar.log
-polybar 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -r 2>&1 | tee -a /tmp/polybar.log & disown
